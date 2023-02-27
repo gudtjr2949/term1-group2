d4d2fb353ce601552ed206fc26a423494553ab1b
hyungseok Lee <gudtjr2949@naver.com>
2023-02-28 00:06:10 +0900

    [백준 2922] 즐거운 단어 풀이 완료

--- HyungSeok/백준/BOJ_2922.java
+++ HyungSeok/백준/BOJ_2922.java
@@ -2,16 +2,18 @@ package coding_test.백준;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 // 백준 2922 : 즐거운 단어
 public class BOJ_2922 {
 
     static String s;
-    static int under_line;
-    static boolean[] Gather = new boolean[26];
+    static boolean[] alpha = new boolean[26];
     static long answer;
-    static ArrayList<Integer> list = new ArrayList<>();
+    static char[] arr = {'A', 'B', 'L'};
+
+    /*
+    순열에서 'A', 'B', 'L' 이 셋 중 하나만 선택 할 수 있게 하기
+     */
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -23,46 +25,74 @@ public class BOJ_2922 {
         char[] mom = {'A', 'E', 'I', 'O', 'U'};
 
         for (int i = 0; i < 5; i++) {
-            Gather[mom[i] - 'A'] = true;
+            alpha[mom[i] - 'A'] = true;
         }
+
+        int cnt = 0;
 
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '_') {
-                list.add(i);
-                under_line++;
+                cnt++;
             }
         }
 
-        perm(0, new char[under_line]);
+        solve(0, new char[cnt]);
 
         System.out.println(answer);
     }
 
-    private static void perm(int idx, char[] input) {
+    private static void solve(int idx, char[] input) {
         if (idx == input.length) {
-            if (L_check(input)) {
-                answer++;
+            if (check(input)) {
+                cnt(input);
             }
             return;
         }
 
-        for (int i = 0; i < 26; i++) {
-            if (check(input, idx)) {
-                input[idx] = (char) (i + 'A');
-                perm(idx + 1, input);
-            }
+        for (int i = 0; i < arr.length; i++) {
+            input[idx] = arr[i];
+            solve(idx + 1, input);
         }
+
     }
 
-    private static boolean L_check(char[] input) {
-        for (int i = 0; i < input.length; i++) {
-            if (input[i] == 'L') {
-                return true;
+    private static boolean check(char[] input) {
+        char[] tmp = s.toCharArray();
+
+        int tmp_idx = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '_') {
+                tmp[i] = input[tmp_idx];
+                tmp_idx++;
             }
         }
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'L') {
+        int mom = 0;
+        int son = 0;
+
+        for (int i = 0; i < tmp.length; i++) {
+            for (int j = i; j < i + 3; j++) {
+                if (j >= tmp.length) {
+                    break;
+                }
+                if (alpha[tmp[j] - 'A']) { // 모음
+                    mom++;
+                } else {
+                    son++;
+                }
+            }
+
+            if (mom == 3 || son == 3) {
+                return false;
+            } else {
+                mom = 0;
+                son = 0;
+            }
+        }
+
+        for (int i = 0; i < tmp.length; i++) {
+            if (tmp[i] == 'L') {
                 return true;
             }
         }
@@ -70,74 +100,17 @@ public class BOJ_2922 {
         return false;
     }
 
-    private static boolean check(char[] input, int idx) {
-        char[] tmp = s.toCharArray();
+    private static void cnt(char[] input) {
+        long sum = 1;
 
-        for (int i = 0; i < idx; i++) {
-            for (int j = 0; j < tmp.length; j++) {
-                if (tmp[j] == '_') {
-                    tmp[j] = input[i];
-                    break;
-                }
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] == 'A') {
+                sum *= 5;
+            } else if (input[i] == 'B') {
+                sum *= 20;
             }
         }
 
-        for (int i = 0; i < idx; i++) {
-            int mom = 0;
-            int son = 0;
-            if (list.get(i) - 2 >= 0) {
-                for (int j = list.get(i) - 2; j <= list.get(i); j++) {
-                    if (tmp[j] != '_') {
-                        if (Gather[tmp[j] - 'A']) {
-                            mom++;
-                        } else {
-                            son++;
-                        }
-                    }
-                }
-
-                if (mom == 3 || son == 3) {
-                    return false;
-                }
-            }
-
-            if (list.get(i) - 1 >= 0 && list.get(i) + 1 < tmp.length) {
-                mom = 0;
-                son = 0;
-                for (int j = list.get(i) - 1; j <= list.get(i) + 1; j++) {
-                    if (tmp[j] != '_') {
-                        if (Gather[tmp[j] - 'A']) {
-                            mom++;
-                        } else {
-                            son++;
-                        }
-                    }
-                }
-
-                if (mom == 3 || son == 3) {
-                    return false;
-                }
-            }
-
-            if (list.get(i) + 2 < tmp.length) {
-                mom = 0;
-                son = 0;
-                for (int j = list.get(i); j <= list.get(i) + 2; j++) {
-                    if (tmp[j] != '_') {
-                        if (Gather[tmp[j] - 'A']) {
-                            mom++;
-                        } else {
-                            son++;
-                        }
-                    }
-                }
-
-                if (mom == 3 || son == 3) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
+        answer += sum;
     }
 }
