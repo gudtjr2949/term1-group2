9b6e73345aa7558737b316d554986ba2979b3aa4
hyungseok Lee <gudtjr2949@naver.com>
2023-03-14 00:02:24 +0900

    [백준 14890] 경사로 풀이 완료

--- HyungSeok/백준/BOJ_14890.java
+++ HyungSeok/백준/BOJ_14890.java
@@ -10,6 +10,7 @@ public class BOJ_14890 {
     static int N, L;
     static int[][] map;
     static int answer;
+    static boolean[][] bridge1, bridge2;
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -20,6 +21,9 @@ public class BOJ_14890 {
 
         map = new int[N][N];
 
+        bridge1 = new boolean[N][N];
+        bridge2 = new boolean[N][N];
+
         for (int i = 0 ; i < N ; i++) {
             st = new StringTokenizer(bf.readLine(), " ");
             for (int j = 0 ; j < N ; j++) {
@@ -28,71 +32,88 @@ public class BOJ_14890 {
         }
 
         // 이동할 방향으로의 높이가 같거나, 현재위치보다 1이 작은데 그 높이가 L 칸 만큼 연속되면 이동 가능
-//        countSameheight();
-
         makeBridge();
 
         System.out.println(answer);
     }
 
-    private static void countSameheight() {
-
-        // 같은 높이가 연속으로 되어있는지 확인
-        for (int i = 0 ; i < N ; i++) {
-            boolean sign = true;
-
-            for (int j = 0 ; j < N-1 ; j++) {
-                if (map[i][j] != map[i][j+1]) {
-                    sign = false;
-                    break;
-                }
-            }
-
-            if (sign) {
-                answer++;
-            }
-        }
-
-        for (int i = 0 ; i < N ; i++) {
-            boolean sign = true;
-
-            for (int j = 0 ; j < N-1 ; j++) {
-                if (map[j][i] != map[j+1][i]) {
-                    sign = false;
-                    break;
-                }
-            }
-
-            if (sign) {
-                answer++;
-            }
-        }
-    }
-    
     // 높이가 1씩 차이나는 길 찾기
     private static void makeBridge() {
-        for (int i = 0 ; i < N ; i++) {
-            int j = 0;
 
+        for (int i = 0 ; i < N ; i++) {
             boolean sign = true;
 
             Loop:
-            while(j < N-L){
-                if (map[i][j] == map[i][j+1]+1) { // 다리를 지을 수 있을 경우
-                    for (int q = j ; q < j+L ; q++) {
-                        if (map[i][j+1] != map[i][q]) {
+            for (int j = 0 ; j < N-1 ; j++) {
+
+                if (map[i][j] == map[i][j+1]) { // 가고자하는 위치의 높이가 같은 경우
+                    continue;
+                }
+                else if (map[i][j] == map[i][j+1]+1) { // 가고자하는 위치의 높이가 1만큼 더 낮은 경우
+                    for (int q = j+1 ; q <= j+L ; q++) {
+                        if (q >= N || map[i][j+1] != map[i][q]) {
                             sign = false;
                             break Loop;
                         }
+
+                        bridge1[i][q] = true;
                     }
-                    j += L;
+
+                    j += L-1;
                 }
-                else if (map[i][j] < map[i][j+1]){
+                else if (map[i][j]+1 == map[i][j+1]) { // 가고자하는 위치의 높이가 1만큼 더 높은 경우
+                    for (int q = j ; q >= j-L+1 ; q--) {
+                        if (q < 0 || map[i][j] != map[i][q] || bridge1[i][q]) {
+                            sign = false;
+                            break Loop;
+                        }
+
+                        bridge1[i][q] = true;
+                    }
+                }
+                else {
                     sign = false;
                     break;
                 }
+            }
+
+            if (sign) {
+                answer++;
+            }
+
+            sign = true;
+
+            Loop:
+            for (int j = 0 ; j < N-1 ; j++) {
+
+                if (map[j][i] == map[j+1][i]) { // 가고자하는 위치의 높이가 같은 경우
+                    continue;
+                }
+                else if (map[j][i] == map[j+1][i]+1) { // 가고자하는 위치의 높이가 1만큼 더 낮은 경우
+                    for (int q = j+1 ; q <= j+L ; q++) {
+                        if (q >= N || map[j+1][i] != map[q][i]) {
+                            sign = false;
+                            break Loop;
+                        }
+
+                        bridge2[q][i] = true;
+                    }
+
+                    j += L-1;
+                }
+                else if (map[j][i]+1 == map[j+1][i]) { // 가고자하는 위치의 높이가 1만큼 더 높은 경우
+                    for (int q = j ; q >= j-L+1 ; q--) {
+                        if (q < 0 || map[j][i] != map[q][i] || bridge2[q][i]) {
+                            sign = false;
+                            break Loop;
+                        }
+
+                        bridge2[q][i] = true;
+                    }
+                }
                 else {
-                    j++;
+                    sign = false;
+                    break;
                 }
             }
 
@@ -101,94 +122,5 @@ public class BOJ_14890 {
             }
         }
 
-        for (int i = 0 ; i < N ; i++) {
-            int j = N-1;
-
-            boolean sign = true;
-
-            Loop:
-            while(j-L >= 0){
-                if (map[i][j] == map[i][j-1]+1) { // 다리를 지을 수 있을 경우
-                    for (int q = j ; q >= j-L ; q--) {
-                        if (map[i][j-1] != map[i][q]) {
-                            sign = false;
-                            break Loop;
-                        }
-                    }
-                    j -= L;
-                }
-                else if (map[i][j] < map[i][j-1]){
-                    sign = false;
-                    break;
-                }
-                else {
-                    j--;
-                }
-            }
-
-            if (sign) {
-                answer++;
-            }
-        }
-
-        for (int i = 0 ; i < N ; i++) {
-            int j = 0;
-
-            boolean sign = true;
-
-            Loop:
-            while(j < N-L){
-                if (map[j][i] == map[j+1][i]+1) { // 다리를 지을 수 있을 경우
-                    for (int q = j ; q < j+L ; q++) {
-                        if (map[j+1][i] != map[q][i]) {
-                            sign = false;
-                            break Loop;
-                        }
-                    }
-                    j += L;
-                }
-                else if (map[j][i] < map[j+1][i]){
-                    sign = false;
-                    break;
-                }
-                else {
-                    j++;
-                }
-            }
-
-            if (sign) {
-                answer++;
-            }
-        }
-
-        for (int i = 0 ; i < N ; i++) {
-            int j = N-1;
-
-            boolean sign = true;
-
-            Loop:
-            while(j-L >= 0){
-                if (map[j][i] == map[j-1][i]+1) { // 다리를 지을 수 있을 경우
-                    for (int q = j ; q >= j-L ; q--) {
-                        if (map[j-1][i] != map[q][i]) {
-                            sign = false;
-                            break Loop;
-                        }
-                    }
-                    j -= L;
-                }
-                else if (map[j][i] < map[j-1][i]){
-                    sign = false;
-                    break;
-                }
-                else {
-                    j--;
-                }
-            }
-
-            if (sign) {
-                answer++;
-            }
-        }
     }
 }
