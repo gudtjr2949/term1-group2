67f0560a60eacb031dbad94de27addbd46b4cd9c
이형석 <gudtjr2949@naver.com>
2023-03-29 21:29:26 +0900

    [백준 11722] 가장 긴 감소하는 부분 수열 풀이 완료

--- HyungSeok/백준/BOJ_11722.java
+++ HyungSeok/백준/BOJ_11722.java
@@ -1,5 +1,8 @@
+package coding_test.백준;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 // 백준 11722 : 가장 긴 감소하는 부분 수열
@@ -10,40 +13,31 @@ public class BOJ_11722 {
 		int N = Integer.parseInt(bf.readLine());
 		int[] A = new int[N];
 		StringTokenizer st = new StringTokenizer(bf.readLine());
-		
+
+		int[] memo = new int[N];
+
 		for (int i = 0 ; i < N ; i++) {
 			A[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		int[][] memo = new int[N][N];
-		
-		for (int i = 0 ; i < N-1 ; i++) {
-			int axis = A[i];
-			
-			for (int j = i+1 ; j < N ; j++) {
-				if (axis > A[j]) {
-					memo[i][j] = axis - A[j];
+
+		Arrays.fill(memo, 1);
+
+		for (int i = 0 ; i < N ; i++) {
+			for (int j = 0 ; j < i ; j++) {
+				if (A[i] < A[j]) {
+					memo[i] = Math.max(memo[j]+1, memo[i]);
 				}
 			}
 		}
-		
+
+		int answer = Integer.MIN_VALUE;
+
 		for (int i = 0 ; i < N ; i++) {
-			for (int j = 0 ; j < N ; j++) {
-				System.out.print(memo[i][j] + " ");
-			}
-			System.out.println();
-		}
-		
-		int[] cnt = new int[N];
-		
-		for (int i = 0 ; i < N ; i++) {
-			int axis = 0;
-			
-			for (int j = 0 ; j < N ; j++) {
-				if (memo[i][j] != 0) {
-					
-				}
+			if (answer < memo[i]) {
+				answer = memo[i];
 			}
 		}
+
+		System.out.println(answer);
 	}
 }
